fcb3756a7a224cfbc570152a7e6a2e450cd8017a
Alpvax <Alpvax@netscape.net>
2015-03-16 00:12:58 +0000

    Modified build-process now run "./build [commit message]" instead of git add/commit/push

--- scripts/build.py
+++ scripts/build.py
@@ -1,37 +1,85 @@
 #!/usr/bin/env python
 import fontforge
 import json
-import os
+import os, sys, getopt
 import re
 basepath = os.path.dirname(__file__)
 def relPath(rpath):
     return os.path.abspath(os.path.join(basepath, rpath))
     
-names = [relPath("/../fonts/rau-utf.ttf")]
-if __name__ == "__main__":
-    fonts = []
+def getVersion(names):
     version = 0
     for i in range(len(names)):
-        print(i)
         if os.path.isfile(names[i]):
             font = fontforge.open(names[i])
         else:
             font = fontforge.font()
-        fonts.append(font)
         version = max(version, int(font.version.split(".")[1]))
-    print("version: " + str(version))
+    return version
+        
+def main():
+    names = [relPath("/../fonts/rau-utf.ttf")]
     data = json.load(open(relPath("../setup_data.json"), 'r'))
     types = data["Rune Types"]
     runes = data["Basic Runes"]
-    for file in os.listdir(relPath("../svgs")):
-        print(file)
-        base = file
-        if re.match("(p(illared)?_?).+", file.lower()) or re.match(".+(_?p(illared)?)", file.lower()):
-            base = re.sub("_?p(illared)?_?", "", file)
-            print("pillared version of " + base)
+    version = getVersion(names)
+    print("version: " + str(version))
+    for i in range(len(names)):
+        font = fontforge.font()
+        font.version = str(i + 1) + "." + version
+        print(font.version)
+        svgPath = relPath("../svgs")
+        for svg in os.listdir(svgPath):
+            print(svg)
+            base = svg
+            imod = 0
+            if re.match("p_.+", svg.lower()):
+                base = svg[2:]
+                print("pillared version of " + base)
+                imod = 1
             if base in runes:
                 rune = runes[base]
-                print(rune)
-                print(rune['type'])
-                print(types[rune['type']])
-            
+                index = rune["index"] + imod + types[rune["type"]][i]
+                print("Adding rune " + svg + " with index " + str(index))
+                glyph = font.createChar(index)
+                glyph.importOutlines(os.path.join(svgPath, svg))
+                flag = True
+        if flag:
+            font.generate(names[i])
+                
+def printUsage(fileName):
+    print("Usage:\t" + fileName + " [options]\n" +
+        "Options:\n" +
+        "-h\t--help\t\tShow this screen\n" +
+        "-m\t--message\tThe message to use for committing. Ignored if -c is used\n" +
+        "-c\t--no-commit\tDo not commit the repository\n" +
+        "-p\t--no-push\tDo not push the repository. Automatically applied by -c\n" +
+        "-r\t--no-rebase\tDo not rebase the repository first\n")
+
+def readArgs(argv):
+    message = "Updated font"
+    commit = True
+    push = True
+    rebase = True
+    try:
+        opts, args = getopt.getopt(argv[1:],"hm:cpr",["help","message=","no-commit","no-push","no-rebase"])
+    except getopt.GetoptError:
+        printUsage(argv[0])
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            printUsage(argv[0])
+            sys.exit()
+        if opt in ("-m", "--message"):
+            message = arg
+        if opt in ("-c", "--no-commit"):
+            commit = False
+        if opt in ("-p", "--no-push", "-c", "--no-commit"):
+            push = False
+        if opt in ("-r", "--no-rebase"):
+            rebase = False
+    return message, commit, push, rebase
+    
+if __name__ == "__main__":
+    #message, c,p,r = readArgs(sys.argv)
+    main()
