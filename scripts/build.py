3d3ab1e92355e87e9e2123d6c24946084755f9ea
Alpvax <Alpvax@netscape.net>
2015-03-22 16:49:28 +0000

    Modified build script to allow custom fonts to be built by merging

--- scripts/build.py
+++ scripts/build.py
@@ -16,73 +16,50 @@ def getVersion(names):
             version = max(version, int(fontforge.open(names[i]).version.split(".")[1]))
     return version
         
+def buildFont(version, name, src=None):
+    if src:
+        font = fontforge.open(src)
+    else:
+        font = fontforge.font()
+    font.familyname = "RAU"
+    font.fontname = font.fullname = name.replace("-","_")
+    font.version = str(version + 1) + "." + str(version)
+    font.copyright = u"Copyright \u00a9 " + str(date.today()) + ", Automatically built."
+    if src:
+        font.copyright += "Based upon " + src.split("/")[-1] + ". Do not distribute."
+    flag = False
+    svgPath = relPath("../svgs")
+    for svg in os.listdir(svgPath):
+        base = rname = svg[:-4]
+        print(base)
+        imod = 0
+        if re.match(".+_p", svg.lower()):
+            base = base[:-2]
+            imod = 1
+        if base in runes:
+            rune = runes[base]
+            index = rune["index"] + imod + types[rune["type"]][version]
+            print("Adding rune " + rname + " with index " + str(index))
+            glyph = font.createChar(index, rname)
+            glyph.importOutlines(os.path.join(svgPath, svg))
+            flag = True
+    if flag:
+        print("Generating font with name: " + name + " and version: " + font.version)
+        font.generate(name)
+        font.close()
+        
 def main():
     names = ["rau-128.ttf","rau-utf.ttf"]
+    version = getVersion(names) + 1
+    for i in range(len(names)):
+        buildFont(i, names[i])
+    
+if __name__ == "__main__":
     data = json.load(open(relPath("../build_data.json"), 'r'))
     types = data["Rune Types"]
     runes = data["Basic Runes"]
-    version = getVersion(names) + 1
-    for i in range(len(names)):
-        font = fontforge.font()
-        font.familyname = "RAU"
-        font.fontname = font.fullname = names[i].replace("-","")
-        print(font.fontname + "; " + font.fullname)
-        font.version = str(i + 1) + "." + str(version)
-        font.copyright = u"Copyright \u00a9 " + str(date.today()) + ", Automatically built."
-        flag = False
-        svgPath = relPath("../svgs")
-        for svg in os.listdir(svgPath):
-            base = name = svg[:-4]
-            print(base)
-            imod = 0
-            if re.match(".+_p", svg.lower()):
-                base = base[:-2]
-                imod = 1
-            if base in runes:
-                rune = runes[base]
-                index = rune["index"] + imod + types[rune["type"]][i]
-                print("Adding rune " + name + " with index " + str(index))
-                glyph = font.createChar(index, name)
-                glyph.importOutlines(os.path.join(svgPath, svg))
-                flag = True
-        if flag:
-            print("Generating font with name: " + names[i] + " and version: " + font.version)
-            font.generate(names[i])
-            font.close()
-                
-def printUsage(fileName):
-    print("Usage:\t" + fileName + " [options]\n" +
-        "Options:\n" +
-        "-h\t--help\t\tShow this screen\n" +
-        "-m\t--message\tThe message to use for committing. Ignored if -c is used\n" +
-        "-c\t--no-commit\tDo not commit the repository\n" +
-        "-p\t--no-push\tDo not push the repository. Automatically applied by -c\n" +
-        "-r\t--no-rebase\tDo not rebase the repository first\n")
-
-def readArgs(argv):
-    message = "Updated font"
-    commit = True
-    push = True
-    rebase = True
-    try:
-        opts, args = getopt.getopt(argv[1:],"hm:cpr",["help","message=","no-commit","no-push","no-rebase"])
-    except getopt.GetoptError:
-        printUsage(argv[0])
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            printUsage(argv[0])
-            sys.exit()
-        if opt in ("-m", "--message"):
-            message = arg
-        if opt in ("-c", "--no-commit"):
-            commit = False
-        if opt in ("-p", "--no-push", "-c", "--no-commit"):
-            push = False
-        if opt in ("-r", "--no-rebase"):
-            rebase = False
-    return message, commit, push, rebase
-    
-if __name__ == "__main__":
-    #message, c,p,r = readArgs(sys.argv)
-    main()
+    if len(sys.argv) > 1:
+        for arg in sys.argv[1:]:
+            buildFont(1, "rau_" + arg.split("/")[-1], src=arg)
+    else:
+        main()
